2445241ca56999baa3f54a137ab36b0fe5ad0bc3
Sam Raker <sam.raker@gmail.com>
2016-07-28 17:10:09 -0400

    add lxml as dependency

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup
 
 
 setup(name="voros",
-      version="0.1.2",
+      version="0.1.3",
       description="scrape MLB pitch API data",
       author="Sam Raker",
       author_email="sam.raker@gmail.com",
       url="https://github.com/swizzard/voros",
       packages=["voros"],
       install_requires=["beautifulsoup4==4.4.1",
+                        "lxml==3.6.1",
                         "requests==2.10.0"])
